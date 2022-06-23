daa22aca62681308ff2e5c0b5695abceb955bbc4
davidnene <davienesh4@gmail.com>
2022-06-23 11:13:43 +0300

    Ad docstrings

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', fetchData)
 
+//fetch data from the API
 function fetchData() {
     fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list')
     .then(response => response.json())
@@ -11,12 +12,13 @@ function displayMeals(data) {
     let meal = meals.slice(0, 30)
     const ingredients = document.getElementById('ingredient')
 
+    //style ul to have 3 columns
     ingredients.style.columns = "70px 4"
     console.log(ingredients)
     let list = document.createElement('li')
     
     
-
+    //Display each ingredient on the page
     meal.forEach(element => {
         let list = document.createElement('li')
         list.textContent = element.strIngredient
@@ -24,14 +26,17 @@ function displayMeals(data) {
         list.classList.toggle("items")
         })
         
+        //grab form from html for submit event listening
         const form = document.getElementById("form")
         form.addEventListener("submit", dispDescription)
         
+        //grab p from html to display description
         const p =document.getElementById("p") 
         function dispDescription(e) {
             e.preventDefault()
             let input = e.target.text.value
 
+            //Configure input to search ingredient
             for (let i of meal) {
                 if (i.strIngredient.toLowerCase() === input.toLowerCase()) {
                     form.reset();
@@ -48,10 +53,3 @@ function displayMeals(data) {
             }      
         }
 }
-
-
-
-
-
-
-
